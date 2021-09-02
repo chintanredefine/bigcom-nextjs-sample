2ac704d41a1198c3fe6f11c927c388d309858397
Chintan Pandya <chintan@redefinesolutions.com>
2021-09-02 20:00:09 +0530

    footer

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -99,7 +99,7 @@ const Footer: FC<Props> = ({ className, pages }) => {
               <div className="social-footer">
                   <h5 className="footer-info-heading">Connect With Us</h5>
                   <h5 className="mobile-soc footer-info-heading">let's connect!</h5>
-                  <Link href="https://www.instagram.com/sleekshop_com/" className="icon icon--instagram">
+                  <Link href="https://www.instagram.com/sleekshop_com/" className="icon--instagram">
                     <a className="text-accent-9 hover:text-accent-6 transition ease-in-out duration-150">
                       Instagram
                     </a>
