6b2976788d7eb53c70ec5a865dfa321bbf79dbaa
Chintan Pandya <chintan@redefinesolutions.com>
2021-09-06 20:35:12 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home({
         ))}
       </Marquee>
       <Hero
-        headline="Headline from Vikas"
+        headline="Headline Here"
         description="This is the details section under main headline section. You can add the text over here. Soufflé bonbon caramels jelly beans."
       />
       <Grid layout="B" variant="filled">
