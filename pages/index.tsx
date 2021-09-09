95e68eba8dd538475555ff2e5e2e788a0b5aae02
Chintan Pandya <chintan@redefinesolutions.com>
2021-09-09 21:17:03 +0530

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,6 @@ export default function Home({
       </Marquee>
       <div className="HomeInstagramWidget-Section">
         <h2 className="page-heading"><a href="https://www.instagram.com/sleekshop_com/" target="_blank" rel="nofollow" title="Sleekshop on Instagram">@sleekshop_com</a></h2>
-            <!-- LightWidget WIDGET -->
         <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
         <iframe src="//lightwidget.com/widgets/5a82391a068b5b14b401c0f2994b3973.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
       </div>
