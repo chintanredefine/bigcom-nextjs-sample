095e9a3d06236632aa254b2eefb4fba038085b07
Chintan Pandya <chintan@redefinesolutions.com>
2021-09-16 19:05:46 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
+import '@assets/common.css'
 
 import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
