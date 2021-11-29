4c4d0a58fda2bbc43b162f427ee3e5d2d78490fa
chandanredefinesolutions <chandan@redefinesolutions.com>
2021-11-29 11:44:16 +0530

    revamping of sleek shop profile section

--- components/ProfileInnerPages/orders.tsx
+++ components/ProfileInnerPages/orders.tsx
@@ -1,6 +1,7 @@
 // import type { GetStaticPropsContext } from 'next'
 import useCustomer from '@framework/customer/use-customer'
 import { useEffect, useState } from 'react'
+import { Bag } from '@components/icons'
 
 export default function Orders() {
   const [orderedItem, setorderedItem] = useState<string[]>([])
@@ -23,49 +24,66 @@ export default function Orders() {
 
   return (
     <>
-      {/* <!-- ---------------------------- component buy it again ---------------------------- --> */}
-      <div className="MainContentInnerdiv mb-2">
-        {/* <!-- title of the Buy It Again Page  --> */}
-        <div className="mainContentChild d-flex justify-content-between">
-          <div className="Heading">Buy It Again</div>
-          <div className="Heading">6 Items</div>
-        </div>
+      {Array.isArray(orderedItem) && orderedItem.length > 0 ? (
+        <>
+          {/* <!-- ---------------------------- component buy it again ---------------------------- --> */}
+          <div className="MainContentInnerdiv mb-2">
+            {/* <!-- title of the Buy It Again Page  --> */}
+            <div className="mainContentChild d-flex justify-content-between">
+              <div className="Heading">Buy It Again</div>
+              <div className="Heading">{orderedItem?.length} Items</div>
+            </div>
 
-        {/* <!-- product list  --> */}
-        <div className="d-flex row">
-          {orderedItem.map((order: any) => {
-            console.log('order ', order)
+            {/* <!-- product list  --> */}
+            <div className="d-flex row">
+              {orderedItem.map((order: any) => {
+                // console.log('order ', order)
 
-            return (
-              <div className="productCard">
-                <div className="productCardImgParent">
-                  <img
-                    className="ProductImg"
-                    src={order?.prod_image[0]?.data[0]?.url_standard}
-                    alt="image not found"
-                  />
-                </div>
-                <div className="Product-Model-Parent">
-                  <p className="Product-Model">SKU: {order.sku}</p>
-                </div>
-                <div>
-                  <p className="Product-Name">{order?.name}</p>
-                </div>
-                <div>
-                  <p className="Product-brand">
-                    {order?.product_options[0]?.display_value}
-                  </p>
-                </div>
-                <div>
-                  <p className="Product-price">$ {order?.prod_price}</p>
-                </div>
-              </div>
-            )
-          })}
-          {/* <!-- end product list  --> */}
-        </div>
-      </div>
-      {/* <!-- ---------------------------- component buy it again ---------------------------- --> */}
+                return (
+                  <div className="productCard">
+                    <div className="productCardImgParent">
+                      <img
+                        className="ProductImg"
+                        src={order?.prod_image[0]?.data[0]?.url_standard}
+                        alt="image not found"
+                      />
+                    </div>
+                    <div className="Product-Model-Parent mt-3">
+                      <p className="Product-Model">SKU: {order.sku}</p>
+                    </div>
+                    <div>
+                      <p className="Product-Name">{order?.name}</p>
+                    </div>
+                    <div>
+                      <p className="Product-brand">
+                        {order?.product_options[0]?.display_value}
+                      </p>
+                    </div>
+                    <div>
+                      <p className="Product-price">$ {order?.prod_price}</p>
+                    </div>
+                  </div>
+                )
+              })}
+              {/* <!-- end product list  --> */}
+            </div>
+          </div>
+          {/* <!-- ---------------------------- component buy it again ---------------------------- --> */}
+        </>
+      ) : (
+        <>
+          <div className="MainContentInnerdiv mb-2">
+            <div className="flex-1 p-24 flex flex-col justify-center items-center ">
+              <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 p-12 bg-primary text-primary">
+                <Bag className="absolute" />
+              </span>
+              <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
+                No Products found
+              </h2>
+            </div>
+          </div>
+        </>
+      )}
     </>
   )
 }
