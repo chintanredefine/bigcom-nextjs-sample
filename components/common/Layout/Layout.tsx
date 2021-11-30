8c59ff54e2ef977fc31c1f294db33226edae1b6f
chandanredefinesolutions <chandan@redefinesolutions.com>
2021-11-30 12:51:48 +0530

    fixing header align which gone at right side

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -109,7 +109,7 @@ const Layout: FC<Props> = ({
         src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
         strategy="beforeInteractive"
       />
-      <div className={cn(s.root)}>
+      <div className={cn(s.root)} style={{ width: '100vw' }}>
         <Navbar links={navBarlinks} />
         <main className="fit MainNewTag">{children}</main>
         <Footer pages={pageProps.pages} />
