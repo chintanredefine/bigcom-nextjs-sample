320909f65bfe1b105366523bdbee21aa5a13c923
chandanredefinesolutions <chandan@redefinesolutions.com>
2021-12-21 19:05:55 +0530

    wishlist functionality done

--- components/ProfileInnerPages/wishlist.tsx
+++ components/ProfileInnerPages/wishlist.tsx
@@ -11,21 +11,9 @@ export default function Wishlist() {
 
   return (
     <div className="MainContentInnerdiv mb-2 orderHistory d-flex justify-content-between">
-      <div className="mt-3 mb-20">
+      <div className="mt-3 mb-20 w-100">
         <div className="group flex flex-col">
           {isLoading || isEmpty ? (
-            // <div className="flex-1 px-12 py-24 flex flex-col justify-center items-center ">
-            //   <span className="border border-dashed border-secondary flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary">
-            //     <Heart className="absolute" />
-            //   </span>
-            //   <h2 className="pt-6 text-2xl font-bold tracking-wide text-center">
-            //     Your wishlist is empty
-            //   </h2>
-            //   <p className="text-accent-6 px-10 text-center pt-2">
-            //     Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
-            //   </p>
-            // </div>
-
             <div className="flex-1 p-24 flex flex-col justify-center items-center ">
               <span className="border border-dashed border-secondary rounded-full flex items-center justify-center w-16 h-16 p-12 bg-primary text-primary">
                 <Heart className="absolute" />
